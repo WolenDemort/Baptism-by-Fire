579de929975633d079d120ebad753091e13cc825
WolenDemort <vorobey0152@gmail.com>
2023-10-07 19:30:55 +0300

    startmenu

---
+++ Assets/Scripts/UI Script/ButtonControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControle : MonoBehaviour
+{
+    public void ButtonScensStart(string sceneName)//метод для запуска любой сцены на выбор
+    {
+        SceneManager.LoadScene(sceneName);//запуск сцены по ее имени
+    }
+
+    public void ButtonExitGame()//метод для закрытия игры в целом
+    {
+        Application.Quit();
+    }
+
+}
